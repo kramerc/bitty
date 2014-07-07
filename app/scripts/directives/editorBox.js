2bc7a2061f02d5b4f74dec470b07a98e632bbac7
Kramer Campbell <kramer@kramerc.com>
2014-07-07 10:10:07 -0700

    Fix editor dirty confirm leave.
    Use $window instead of window. Use the jQuery element of $window to
    bind listeners. Fix final location after modal confirm while properly
    retaining browser history.

--- app/scripts/directives/editorBox.js
+++ app/scripts/directives/editorBox.js
@@ -1,57 +1,75 @@
 'use strict';
 
-angular.module('bitty').directive('editorBox', function ($location, $modal) {
-  return {
-    restrict: 'AE',
-    link: function (scope, element) {
-      var confirmMsg = 'You will lose any unsaved changes.';
+angular.module('bitty')
+  .directive('editorBox', function ($location, $modal, $window) {
+    return {
+      restrict: 'AE',
+      link: function (scope, element) {
+        var confirmMsg = 'You will lose any unsaved changes.';
+        var popping = false;
+        var win = angular.element($window);
 
-      element.addClass('editor-box');
+        element.addClass('editor-box');
 
-      scope.dirty = false;
+        scope.dirty = false;
 
-      scope.aceChanged = function () {
-        scope.dirty = true;
-      };
+        scope.aceChanged = function () {
+          scope.dirty = true;
+        };
 
-      scope.aceLoaded = function (editor) {
-        editor.setShowPrintMargin(false);
-      };
+        scope.aceLoaded = function (editor) {
+          editor.setShowPrintMargin(false);
+        };
 
-      window.onbeforeunload = function () {
-        if (scope.dirty) {
-          return confirmMsg;
-        }
-      };
-
-      element.bind('$destroy', function () {
-        window.onbeforeunload = null;
-      });
-
-      scope.$on('$locationChangeStart', function (event, next) {
-        if (!scope.dirty) {
-          return;
-        }
-
-        event.preventDefault();
-        $modal.open({
-          templateUrl: 'templates/modals/confirm.html',
-          controller: 'ConfirmCtrl',
-          resolve: {
-            confirm: function () {
-              return {
-                title: 'Are you sure?',
-                body: confirmMsg
-              };
-            }
+        function beforeunloadListener() {
+          if (scope.dirty) {
+            return confirmMsg;
           }
-        }).result.then(function () {
-          // Confirmed
-          scope.dirty = false;
-          $location.url(next);
+        }
+
+        function popstateListener() {
+          popping = true;
+        }
+
+        win.bind('beforeunload', beforeunloadListener);
+        win.bind('popstate', popstateListener);
+
+        element.bind('$destroy', function () {
+          win.unbind('beforeunload', beforeunloadListener);
+          win.unbind('popstate', popstateListener);
         });
-      });
-    },
-    templateUrl: 'templates/directives/editorBox.html'
-  };
-});
+
+        scope.$on('$locationChangeStart', function (event, next) {
+          if (!scope.dirty) {
+            return;
+          }
+
+          event.preventDefault();
+          $modal.open({
+            templateUrl: 'templates/modals/confirm.html',
+            controller: 'ConfirmCtrl',
+            resolve: {
+              confirm: function () {
+                return {
+                  title: 'Are you sure?',
+                  body: confirmMsg
+                };
+              }
+            }
+          }).result.then(function () {
+            // Confirmed
+            scope.dirty = false;
+
+            if (popping) {
+              $window.history.back();
+            } else {
+              $location.$$parse(next);
+            }
+          }, function () {
+            popping = false;
+          });
+        });
+      },
+      templateUrl: 'templates/directives/editorBox.html'
+    };
+  });
