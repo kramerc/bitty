5008b1d7f76e6935c415c85d07ff62502119a762
Kramer Campbell <kramer@kramerc.com>
2014-07-10 20:52:44 -0700

    Remove unneeded renderer in markedGist directive.

--- app/scripts/directives/markedGist.js
+++ app/scripts/directives/markedGist.js
@@ -6,28 +6,7 @@ angular.module('bitty').directive('markedGist', function (marked) {
     replace: true,
     scope: false,
     link: function (scope, element) {
-      var renderer = new marked.Renderer();
       var taskElements;
-
-      renderer.list = function (body, ordered) {
-        var type = ordered ? 'ol' : 'ul';
-        var list = '<' + type + '>\n' + body + '</' + type + '>\n';
-        if (/<li class="task-list-item">/.test(body)) {
-          list = list.replace(
-            '<' + type + '>', '<' + type + ' class="task-list">');
-        }
-        return list;
-      };
-      renderer.listitem = function (text) {
-        if (/^\s*\[[x ]\]\s*/.test(text)) {
-          text = text
-            .replace(/^\s*\[ \]\s*/, '<input type="checkbox"> ')
-            .replace(/^\s*\[x\]\s*/, '<input type="checkbox" checked> ');
-          return '<li class="task-list-item">' + text + '</li>';
-        }
-
-        return '<li>' + text + '</li>';
-      };
 
       function enableDisableItems() {
         if (!scope.updating && scope.gist.user &&
@@ -40,8 +19,7 @@ angular.module('bitty').directive('markedGist', function (marked) {
 
       function parse(val) {
         element.html(marked(val, {
-          breaks: false,
-          renderer: renderer
+          breaks: false
         }));
 
         taskElements = element.find('.task-list-item input');
