12100c8569795d3d660f28150eb4854d0dd9fd72
Kramer Campbell <kramer@kramerc.com>
2014-07-14 16:27:24 -0700

    Simplify setting the container class.

--- app/scripts/services/layout.js
+++ app/scripts/services/layout.js
@@ -2,10 +2,26 @@
 
 angular.module('bitty').service('layout', function ($rootScope) {
   var defaults = {
+    containerClass: 'container',
     editor: false,
     fluid: false,
     navbar: true
   };
+  var fluid = defaults.fluid;
+
+  Object.defineProperty(this, 'fluid', {
+    get: function () {
+      return fluid;
+    }.bind(this),
+    set: function (value) {
+      if (value) {
+        this.containerClass = 'container-fluid';
+      } else {
+        this.containerClass = 'container';
+      }
+      fluid = value;
+    }.bind(this)
+  });
 
   $rootScope.$on('$stateChangeStart', function () {
     for (var setting in defaults) {
